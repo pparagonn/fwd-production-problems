8ce1625c08fb4188851db40d48c31e7efc7a841b
Jovany Melchor <jmelchor@hawk.iit.edu>
2021-11-22 21:12:17 -0600

    Complete step 2 by using let

--- pp-04/site.js
+++ pp-04/site.js
@@ -24,7 +24,15 @@
   can accurately test your work.
 */
 
+let x = 5;
 
+let double = function(num) {
+  let x = num * 2;
+  return x;
+}
+
+double(6);
+console.log('The value of x is', x, '-- it should be 5.');
 /*
   Step 3. Rewrite your corrected `double()` function from Step 2.
   so that non-number values passed into the function are handled
